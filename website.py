408c7714b66b71e5d1ce8cfb19996b745b2b7225
Daniel Habib <dghabib7@gmail.com>
2021-06-08 10:27:55 -0700

    Dan Splash site

---
+++ website.py
@@ -0,0 +1,16 @@
+# website.py
+
+from splashgen import launch
+from splashgen.components import SplashSite, CTAButton
+
+site = SplashSite(title="DanSplash - Test Splash Pages Built In Python by Dan",
+                  theme="dark")
+site.headline = "Dan built this page"
+site.subtext = """
+DANDANDNADN ANANDNADNANDANDNADNADNANDA NDNADNADNNADNADNANDN
+DANDANDNA
+DANDNAND"""
+site.call_to_action = CTAButton(
+    "https://github.com/danielhabib", "View on GitHub")
+
+launch(site)
